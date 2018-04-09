effd975458f15a042864236bca7ebc3c18a76809
Oleg Kleiman <oleg_kleyman@hotmail.com>
2018-04-09 03:07:18 +0300

    Update AuthMessageInspector.cs

--- AuthMessageInspector.cs
+++ AuthMessageInspector.cs
@@ -1,6 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Dispatcher;
@@ -11,6 +12,9 @@ namespace AuthMapper
 {
     public class AuthMessageInspector : IClientMessageInspector 
     {
+        private readonly string _username = "olegk";
+        private readonly string _password = "dfnc94^*";
+
         public void AfterReceiveReply(ref Message reply, object correlationState)
         {
             Console.WriteLine("AfterReceiveReply called");
@@ -21,8 +25,37 @@ namespace AuthMapper
             // Implement this method to inspect/modify messages before they   
             // are sent to the service 
 
+            HttpRequestMessageProperty httpRequest = null;
+            if (request.Properties.ContainsKey(HttpRequestMessageProperty.Name))
+            {
+                httpRequest = request.Properties[HttpRequestMessageProperty.Name] as HttpRequestMessageProperty;
+            }
+
+            //Make sure we have a valid property, or create one
+            if (httpRequest == null)
+            {
+                httpRequest = new HttpRequestMessageProperty();
+                request.Properties.Add(HttpRequestMessageProperty.Name, httpRequest);
+            }
+
+            using (OperationContextScope scope = new OperationContextScope((IContextChannel)channel))
+            {
+            
+            }
+
             Console.WriteLine("BeforeSendRequest called");
+
+            string encodedCredentials = EncodeCredentials(_username, _password);
+            httpRequest.Headers[HttpRequestHeader.Authorization] = "Basic " + encodedCredentials;
+
             return null;
+        }
+
+        private string EncodeCredentials(string username, string password)
+        {
+            string clearTextCredentials = string.Format("{0}:{1}", username, password);
+            var inArray = Encoding.UTF8.GetBytes(clearTextCredentials);
+            return Convert.ToBase64String(inArray);
         }
 
 
